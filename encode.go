1c771e2bf44e85d433649a9cbcf6d6e5a4b832a0
Sean <rseanjustice@gmail.com>
2025-06-08 10:01:29 -0700

    Encoding (#1)
    * adding the encoding of the logging data
    
    * adding map encode unit test
    
    * added edge cases with a failing test
    
    * added edge cases with a failing test
    
    * array unit tests
    
    * array edge case
    
    * encode struct unit tests
    
    * encode struct unit tests
    
    * handle time

---
+++ encode.go
@@ -0,0 +1,212 @@
+package logit
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func encode(input any) (any, error) {
+	v := reflect.ValueOf(input)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if k := v.Kind(); k != reflect.Map && k != reflect.Struct {
+		return nil, fmt.Errorf("input must be a map or struct but is %v", k)
+	}
+	switch v.Kind() {
+	case reflect.Map:
+		return encodeMap(v)
+	default:
+		return encodeStruct(v)
+	}
+}
+
+func encodeMap(input reflect.Value) (map[string]any, error) {
+	if k := input.Kind(); k != reflect.Map {
+		return nil, fmt.Errorf("input must be a map but is %v", k)
+	}
+
+	if k := input.Type().Key().Kind(); k != reflect.String {
+		return nil, fmt.Errorf("map keys must be strings but are %v", k)
+	}
+
+	resultMap := map[string]any{}
+
+	iter := input.MapRange()
+	for iter.Next() {
+		mapKey := iter.Key()
+
+		var key string
+		kk := mapKey.Kind()
+		switch kk {
+		case reflect.String:
+			key = mapKey.String()
+		default:
+			return nil, fmt.Errorf("map keys must be strings but are %v", kk)
+		}
+
+		mapValue := iter.Value()
+		if mapValue.Kind() == reflect.Interface {
+			mapValue = reflect.ValueOf(mapValue.Interface())
+		}
+		if mapValue.Kind() == reflect.Ptr {
+			mapValue = mapValue.Elem()
+		}
+		if !mapValue.IsValid() {
+			return nil, fmt.Errorf("the map key %s value, %v, is not valid or supported at this time", key, mapValue.Kind())
+		}
+		if v, has := retrieveValue(mapValue); has {
+			resultMap[key] = v
+			continue
+		}
+		val := mapValue.Interface()
+		switch mapValue.Kind() {
+		case reflect.Map:
+			encodedVal, err := encodeMap(reflect.ValueOf(val))
+			if err != nil {
+				return nil, errors.Join(fmt.Errorf("the map key %s had an error encoding a map", key), err)
+			}
+			resultMap[key] = encodedVal
+		case reflect.Array, reflect.Slice:
+			encodedVal, err := encodeArray(reflect.ValueOf(val))
+			if err != nil {
+				return nil, errors.Join(fmt.Errorf("the map key %s had an error encoding an array or slice", key), err)
+			}
+			resultMap[key] = encodedVal
+		case reflect.Struct:
+			encodedVal, err := encodeStruct(reflect.ValueOf(val))
+			if err != nil {
+				return nil, errors.Join(fmt.Errorf("the map key %s had an error encoding a struct", key), err)
+			}
+			resultMap[key] = encodedVal
+		default:
+			resultMap[key] = val
+		}
+	}
+
+	return resultMap, nil
+}
+
+func encodeArray(input reflect.Value) ([]any, error) {
+	if input.Kind() != reflect.Slice && input.Kind() != reflect.Array {
+		return nil, fmt.Errorf("input should be a slice or array, but got %v", input.Kind())
+	}
+
+	var resultArray []any
+
+	for i := 0; i < input.Len(); i++ {
+		val := input.Index(i)
+		if val.Kind() == reflect.Ptr {
+			val = val.Elem()
+		}
+		if v, has := retrieveValue(val); has {
+			resultArray = append(resultArray, v)
+			continue
+		}
+		switch val.Kind() {
+		case reflect.Map:
+			encodedVal, err := encodeMap(val)
+			if err != nil {
+				return nil, err
+			}
+			resultArray = append(resultArray, encodedVal)
+		case reflect.Struct:
+			encodedVal, err := encodeStruct(val)
+			if err != nil {
+				return nil, err
+			}
+			resultArray = append(resultArray, encodedVal)
+		case reflect.Array, reflect.Slice:
+			encodedVal, err := encodeArray(val)
+			if err != nil {
+				return nil, err
+			}
+			resultArray = append(resultArray, encodedVal)
+		default:
+			resultArray = append(resultArray, val.Interface())
+		}
+	}
+
+	return resultArray, nil
+}
+
+func encodeStruct(input reflect.Value) (map[string]any, error) {
+	if input.Kind() != reflect.Struct {
+		return nil, errors.New("input should be a struct")
+	}
+	if input.Kind() == reflect.Ptr {
+		input = input.Elem()
+	}
+
+	resultMap := make(map[string]any)
+
+	for i := 0; i < input.NumField(); i++ {
+		field := input.Type().Field(i)
+		fieldValue := input.Field(i)
+
+		key := strings.ToLower(field.Name) // Default key is lowercase field name
+		tagValue := field.Tag.Get("logit")
+		tagOptions := strings.Split(tagValue, ",")
+		if len(tagOptions) > 0 && len(tagOptions[0]) > 0 {
+			key = tagOptions[0] // Use the first tag option as key if available
+		}
+
+		if len(tagOptions) > 1 && tagOptions[1] == "omitempty" {
+			continue // Skip if the omitempty tag is present and field value is zero
+		}
+		if fieldValue.CanInterface() && fieldValue.Kind() == reflect.Ptr {
+			fieldValue = fieldValue.Elem()
+		}
+		if val, has := retrieveValue(fieldValue); has {
+			resultMap[key] = val
+			continue
+		}
+		switch fieldValue.Kind() {
+		case reflect.Array, reflect.Slice:
+			arrVale, err := encodeArray(fieldValue)
+			if err != nil {
+				return nil, err
+			}
+			resultMap[key] = arrVale
+		case reflect.Map:
+			mapVal, err := encodeMap(fieldValue)
+			if err != nil {
+				return nil, err
+			}
+			resultMap[key] = mapVal
+		case reflect.Struct:
+			mapVal, err := encodeStruct(fieldValue)
+			if err != nil {
+				return nil, err
+			}
+			resultMap[key] = mapVal
+		default:
+			if fieldValue.CanInterface() {
+				resultMap[key] = fieldValue.Interface()
+			}
+		}
+	}
+
+	return resultMap, nil
+}
+
+func retrieveValue(input reflect.Value) (any, bool) {
+	if !input.CanInterface() {
+		return nil, false
+	}
+	if input.Kind() == reflect.Ptr {
+		input = input.Elem()
+	}
+	val := input.Interface()
+	switch v := val.(type) {
+	case time.Time:
+		return v.Format(time.RFC3339), true
+	case *time.Time:
+		return v.Format(time.RFC3339), true
+	default:
+		return nil, false
+	}
+}
---
+++ encode_test.go
@@ -0,0 +1,481 @@
+package logit
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testAttributes struct {
+	Attr1         string `logit:"attribute_one"`
+	Attr2         int    `logit:",omitempty"`
+	Attr3         []string
+	notGoingToLog string
+}
+
+func Test_encodeMap(t *testing.T) {
+	type args struct {
+		input any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]any
+		wantErr bool
+	}{
+		{
+			name: "Testing map encoding",
+			args: args{
+				input: map[string]any{
+					"key_ptr": func() *string {
+						p := "string pointer"
+						return &p
+					}(),
+					"key1": "value1",
+					"key2": 123,
+					"key3": []any{"1", "2"},
+					"key4": map[string]any{
+						"sub_key1": "sub value1",
+						"sub_key2": 3455,
+					},
+					"key5": testAttributes{
+						Attr1:         "struct attribute",
+						Attr2:         99887,
+						Attr3:         []string{"elem1", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+					"key6": []testAttributes{
+						{
+							Attr1:         "struct attribute array 1",
+							Attr2:         9988455557,
+							Attr3:         []string{"elem1_1", "elem2_1"},
+							notGoingToLog: "this should not be logged",
+						},
+						{
+							Attr1:         "struct attribute array 2",
+							Attr2:         998444487,
+							Attr3:         []string{"elem1_2", "elem2_2"},
+							notGoingToLog: "this should not be logged",
+						},
+					},
+				},
+			},
+			want: map[string]any{
+				"key_ptr": "string pointer",
+				"key1":    "value1",
+				"key2":    123,
+				"key3":    []any{"1", "2"},
+				"key4": map[string]any{
+					"sub_key1": "sub value1",
+					"sub_key2": 3455,
+				},
+				"key5": map[string]any{
+					"attribute_one": "struct attribute",
+					"attr3":         []any{"elem1", "elem2"},
+				},
+				"key6": []any{
+					map[string]any{
+						"attribute_one": "struct attribute array 1",
+						"attr3":         []any{"elem1_1", "elem2_1"},
+					},
+					map[string]any{
+						"attribute_one": "struct attribute array 2",
+						"attr3":         []any{"elem1_2", "elem2_2"},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid nested map key kind",
+			args: args{
+				input: map[string]any{
+					"key1": "value1",
+					"key2": map[int]any{
+						1: "should be an error",
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Invalid input type",
+			args: args{
+				input: struct{ Field string }{"test"},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Non-string keys in map",
+			args: args{
+				input: map[int]any{1: "value1"},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Nested structure with unsupported type",
+			args: args{
+				input: map[string]any{"key1": time.Date(2025, time.May, 20, 12, 33, 15, 8, time.UTC)},
+			},
+			want: map[string]any{
+				"key1": "2025-05-20T12:33:15Z",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Circular reference detection",
+			args: args{
+				input: map[string]any{"key1": nil},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := reflect.ValueOf(tt.args.input)
+			if val.Kind() == reflect.Ptr {
+				val = val.Elem()
+			}
+
+			got, err := encodeMap(val)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encodeMap() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_encodeArray(t *testing.T) {
+	type args struct {
+		input any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []any
+		wantErr bool
+	}{
+		{
+			name: "Succes, raw array",
+			args: args{
+				input: []int{8, 6, 2, 3, 4},
+			},
+			want:    []any{8, 6, 2, 3, 4},
+			wantErr: false,
+		},
+		{
+			name: "Succes, raw array double",
+			args: args{
+				input: [][]int{{8, 6}, {2, 3}},
+			},
+			want:    []any{[]any{8, 6}, []any{2, 3}},
+			wantErr: false,
+		},
+		{
+			name: "Succes, map array",
+			args: args{
+				input: []map[string]any{
+					{
+						"key1": "something one",
+						"key2": 2,
+						"key4": true,
+					},
+					{
+						"key1": "something two",
+						"key2": 7,
+						"key4": true,
+					},
+					{
+						"key1": "something three",
+						"key2": 100,
+						"key4": false,
+					},
+				},
+			},
+			want: []any{
+				map[string]any{
+					"key1": "something one",
+					"key2": 2,
+					"key4": true,
+				},
+				map[string]any{
+					"key1": "something two",
+					"key2": 7,
+					"key4": true,
+				},
+				map[string]any{
+					"key1": "something three",
+					"key2": 100,
+					"key4": false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Succes, struct array",
+			args: args{
+				input: []testAttributes{
+					{
+						Attr1:         "struct attribute one",
+						Attr2:         99887,
+						Attr3:         []string{"elem1", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+					{
+						Attr1:         "struct attribute two",
+						Attr2:         1000,
+						Attr3:         []string{"elem1_", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+					{
+						Attr1:         "struct attribute three",
+						Attr2:         5,
+						Attr3:         []string{"elem1", "elem2__"},
+						notGoingToLog: "this should not be logged",
+					},
+				},
+			},
+			want: []any{
+				map[string]any{
+					"attribute_one": "struct attribute one",
+					"attr3":         []any{"elem1", "elem2"},
+				},
+				map[string]any{
+					"attribute_one": "struct attribute two",
+					"attr3":         []any{"elem1_", "elem2"},
+				},
+				map[string]any{
+					"attribute_one": "struct attribute three",
+					"attr3":         []any{"elem1", "elem2__"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail: not an array",
+			args: args{
+				input: "not an array",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := reflect.ValueOf(tt.args.input)
+			if val.Kind() == reflect.Ptr {
+				val = val.Elem()
+			}
+
+			got, err := encodeArray(val)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encodeArray() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_encodeStruct(t *testing.T) {
+	type testStruct struct {
+		SomeMap    map[string]any `logit:"some_map"`
+		SomeArray  []int
+		SomeStruct testAttributes
+	}
+	type args struct {
+		input any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]any
+		wantErr bool
+	}{
+		{
+			name: "Success",
+			args: args{
+				input: testStruct{
+					SomeMap: map[string]any{
+						"some_map_key":   1,
+						"some_map_key_2": "some map string",
+					},
+					SomeArray: []int{9, 4, 1},
+					SomeStruct: testAttributes{
+						Attr1:         "struct attribute one",
+						Attr2:         99887,
+						Attr3:         []string{"elem1", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+				},
+			},
+			want: map[string]any{
+				"some_map": map[string]any{
+					"some_map_key":   1,
+					"some_map_key_2": "some map string",
+				},
+				"somearray": []any{9, 4, 1},
+				"somestruct": map[string]any{
+					"attribute_one": "struct attribute one",
+					"attr3":         []any{"elem1", "elem2"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail: Not a struct",
+			args: args{
+				input: 1,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := reflect.ValueOf(tt.args.input)
+			if val.Kind() == reflect.Ptr {
+				val = val.Elem()
+			}
+
+			got, err := encodeStruct(val)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encodeStruct() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_encode(t *testing.T) {
+	type testStruct struct {
+		SomeMap    map[string]any `logit:"some_map"`
+		SomeArray  []int
+		SomeStruct testAttributes
+	}
+	type args struct {
+		input any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    any
+		wantErr bool
+	}{
+		{
+			name: "Success: Struct",
+			args: args{
+				input: testStruct{
+					SomeMap: map[string]any{
+						"some_map_key":   1,
+						"some_map_key_2": "some map string",
+					},
+					SomeArray: []int{9, 4, 1},
+					SomeStruct: testAttributes{
+						Attr1:         "struct attribute one",
+						Attr2:         99887,
+						Attr3:         []string{"elem1", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+				},
+			},
+			want: map[string]any{
+				"some_map": map[string]any{
+					"some_map_key":   1,
+					"some_map_key_2": "some map string",
+				},
+				"somearray": []any{9, 4, 1},
+				"somestruct": map[string]any{
+					"attribute_one": "struct attribute one",
+					"attr3":         []any{"elem1", "elem2"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Succes: Map",
+			args: args{
+				input: map[string]any{
+					"key1": "value1",
+					"key2": 123,
+					"key3": []any{"1", "2"},
+					"key4": map[string]any{
+						"sub_key1": "sub value1",
+						"sub_key2": 3455,
+					},
+					"key5": testAttributes{
+						Attr1:         "struct attribute",
+						Attr2:         99887,
+						Attr3:         []string{"elem1", "elem2"},
+						notGoingToLog: "this should not be logged",
+					},
+					"key6": []testAttributes{
+						{
+							Attr1:         "struct attribute array 1",
+							Attr2:         9988455557,
+							Attr3:         []string{"elem1_1", "elem2_1"},
+							notGoingToLog: "this should not be logged",
+						},
+						{
+							Attr1:         "struct attribute array 2",
+							Attr2:         998444487,
+							Attr3:         []string{"elem1_2", "elem2_2"},
+							notGoingToLog: "this should not be logged",
+						},
+					},
+				},
+			},
+			want: map[string]any{
+				"key1": "value1",
+				"key2": 123,
+				"key3": []any{"1", "2"},
+				"key4": map[string]any{
+					"sub_key1": "sub value1",
+					"sub_key2": 3455,
+				},
+				"key5": map[string]any{
+					"attribute_one": "struct attribute",
+					"attr3":         []any{"elem1", "elem2"},
+				},
+				"key6": []any{
+					map[string]any{
+						"attribute_one": "struct attribute array 1",
+						"attr3":         []any{"elem1_1", "elem2_1"},
+					},
+					map[string]any{
+						"attribute_one": "struct attribute array 2",
+						"attr3":         []any{"elem1_2", "elem2_2"},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail",
+			args: args{
+				input: []int{1, 2, 3},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := encode(tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
